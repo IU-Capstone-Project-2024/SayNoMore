9773107a686d274bcca4cf0ef67bb184d32e5233
paket2004 <103316763+paket2004@users.noreply.github.com>
2024-06-10 20:56:07 +0300

    Update arrival_retriever.py

--- request_analyzer/retreivers/arrival_retriever.py
+++ request_analyzer/retreivers/arrival_retriever.py
@@ -14,37 +14,36 @@ class ArrivalRetriever(BaseRetriever):
         self.llm = llm
         # Setting up sampling parameters for deterministic output
         self.sampling_params = SamplingParams(temperature=0, stop="\n")
-        # Defining a prompt template to guide the model towards
-        # extracting arrival cities
+        # Defining a prompt template to guide the model extract arrival cities from the user input
         self.cur_day = datetime.now()
         self.prefix_prompt = \
             '''Today is June 9, 2024. Sunday. Your task is to extract the arrival time to the destination city from the user's request. Examples:
             Q: "Планирую сгонять в Хабаровск через три недели."
             A: Arrival Time: "30/06/2024"
-    
+
             Q: "Хочу уехать из Москвы куда-нибудь на три дня, есть двадцать тысяч"
             A: Arrival Time: "None"
-    
+
             Q: "Уеду в Питер из Казани в июле с 12 по 17 числа +- 300000 рублей"
             A: Arrival Time: "12/07/2024"
-    
+
             Q: "Уеду в Москву из Рязани в августе с 10 по 30. Бюджет 70 тысяч."
             A: Arrival Time: "10/08/2024"
-    
+
             Q: "Уеду из Рязани в августе с 10 по 30. Есть 70 тысяч."
             A: Arrival Time: "10/08/2024"
-    
+
             Q: "Я в Тольятти. Мне срочно надо достать билеты в Кисловодск"
             A: Arrival Time: "None"
-    
+
             Q: "Я в Москву в среду"
             A: Arrival Time: "12/06/2024"
-    
+
             Q: "Я в Москву в с 1ое по 5ое мая"
             A: Arrival Time: "01/05/2025"
-    
-            Today is {self.cur_day.strftime('%B %d %Y')} Your task is to extract the arrival time to the destination city from the user's request.
-    
+
+            Today is {self.cur_day.strftime('%B %d %Y. %A'). } Your task is to extract the arrival time to the destination city from the user's request.
+
             Q: "USER_REQUEST"
             A: Arrival Time: "'''
 
@@ -59,7 +58,7 @@ class ArrivalRetriever(BaseRetriever):
                            request as a string.
 
         Returns:
-            str: The extracted destination city
+            str: The extracted arrival city
                  as a string, or a message indicating
                  no destination was found.
         """
@@ -72,5 +71,5 @@ class ArrivalRetriever(BaseRetriever):
         vllm_output = self.llm.generate(prompt,
                                         self.sampling_params)
         # Extract and return the generated text as
-        # the destination city
+        # the arrival city
         return vllm_output[0].outputs[0].text
