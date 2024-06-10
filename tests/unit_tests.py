86f1db47fc44b348caa46530dcd050416469d58a
Roman Makeev <r.makeev@innopolis.university>
2024-06-10 13:41:51 +0300

    feat: unit test for 'test_fetch_grouped_tickets' was created

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from air_tickets.air_tickets_api import AirTicketsApi
+from datetime import datetime, timedelta
 
 class TestAirTicketsApi(TestCase):
     def setUp(self):
@@ -15,6 +16,22 @@ class TestAirTicketsApi(TestCase):
         self.assertTrue(response['success'])
         self.assertEqual(response['data'][0]['origin'], expected_data['origin'])
         self.assertEqual(response['data'][0]['destination'], expected_data['destination'])
+
+    def test_fetch_grouped_tickets(self):
+        expected_data = {
+            'origin': "MOW",
+            'destination': "DXB"
+        }
+        today = datetime.today()
+        two_weeks_later = today + timedelta(weeks=2)
+        departure_date = two_weeks_later.strftime('%Y-%m-%d')
+        print(departure_date)
+
+        response = self.api.fetch_grouped_tickets(origin='MOW', destination='DXB', departure_at=departure_date)
+        print(response)
+        self.assertTrue(response['success'])
+        self.assertEqual(response['data'][departure_date]['origin'], expected_data['origin'])
+        self.assertEqual(response['data'][departure_date]['destination'], expected_data['destination'])
 
 if __name__ == '__main__':
     main()
