8955364c91b82e80e09746959dd2ca31c36561d7
Elisei Smirnov <eliwhatthe@gmail.com>
2024-06-11 07:10:48 +0000

    added similarity search

--- request_analyzer/retreivers/destination_retriever.py
+++ request_analyzer/retreivers/destination_retriever.py
@@ -1,6 +1,6 @@
 from vllm import LLM, SamplingParams
 from request_analyzer.retreivers.abstract_retriever import BaseRetriever
-
+from request_analyzer.utils.embedding_city_search import EmbeddingCitySearch
 
 class DestinationRetriever(BaseRetriever):
     """
@@ -19,7 +19,7 @@ class DestinationRetriever(BaseRetriever):
                    requests.
     """
 
-    def __init__(self, llm: LLM) -> None:
+    def __init__(self, llm: LLM, searcher: EmbeddingCitySearch) -> None:
         """
         Initializes the DestinationRetriever with
         a given VLLM instance and sets up default 
@@ -28,10 +28,14 @@ class DestinationRetriever(BaseRetriever):
         Args:
             llm (LLM): The VLLM instance to be used
                        for text generation.
+            searcher (EmbeddingCitySearch): Class, to
+                       search for all Russian cities 
+                       to ignore grammatical errors 
+                       entered by the user
         """
         self.llm = llm
         # Setting up sampling parameters for deterministic output
-        self.sampling_params = SamplingParams(temperature=0, stop="\n")
+        self.sampling_params = SamplingParams(temperature=0, stop='"')
         # Defining a prompt template to guide the model towards
         # extracting destination cities
         self.prefix_prompt = \
@@ -57,6 +61,8 @@ class DestinationRetriever(BaseRetriever):
         Q: "USER_REQUEST"
         A: Destination: "'''
 
+        self.searcher = searcher
+
     def retrieve(self, request: str) -> str:
         """
         Generates a response from the VLLM based 
@@ -80,6 +86,8 @@ class DestinationRetriever(BaseRetriever):
         # the customized prompt and sampling parameters
         vllm_output = self.llm.generate(prompt, 
                                         self.sampling_params)
-        # Extract and return the generated text as 
-        # the destination city
-        return vllm_output[0].outputs[0].text
+        extracted_city = vllm_output[0].outputs[0].text
+        if not extracted_city == 'None':
+            found_russian_city = self.searcher.search_city(extracted_city)
+            return found_russian_city
+        return extracted_city
