183144d5808b42daf68360402b36fc8a7d5fb1dd
Roman Makeev <r.makeev@innopolis.university>
2024-06-12 13:52:17 +0300

    feat: method 'test_search_hotel_or_location' has been added

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from air_tickets.air_tickets_api import AirTicketsApi
+from hotels.hotel_api import HotelApi
 from datetime import datetime, timedelta
 import os
 
@@ -8,7 +9,8 @@ import os
 class TestAirTicketsApi(TestCase):
     def setUp(self):  # This method is called before each test method is executed
         # Initialize an instance of AirTicketsApi for use in the tests
-        self.api = AirTicketsApi()
+        self.air_api = AirTicketsApi()
+        self.hotel_api = HotelApi()
 
     def test_fetch_cheapest_tickets(self):  # Test method for fetching cheapest tickets
         # Define expected data structure for comparison
@@ -18,7 +20,7 @@ class TestAirTicketsApi(TestCase):
         }
 
         # Call the fetch_cheapest_tickets method on the api object and store its response
-        response = self.api.fetch_cheapest_tickets(origin='MOW', destination='DXB')
+        response = self.air_api.fetch_cheapest_tickets(origin='MOW', destination='DXB')
 
         # Assert that the response indicates success
         self.assertTrue(response['success'])
@@ -39,7 +41,7 @@ class TestAirTicketsApi(TestCase):
         departure_date = two_weeks_later.strftime('%Y-%m-%d')
 
         # Call the fetch_grouped_tickets method on the api object and store its response
-        response = self.api.fetch_grouped_tickets(origin='MOW', destination='DXB', departure_at=departure_date)
+        response = self.air_api.fetch_grouped_tickets(origin='MOW', destination='DXB', departure_at=departure_date)
 
         # Assert that the response indicates success
         self.assertTrue(response['success'])
@@ -60,7 +62,7 @@ class TestAirTicketsApi(TestCase):
         beginning_of_period = two_weeks_later.strftime('%Y-%m-%d')
 
         # Call the fetch_period_tickets method on the api object and store its response
-        response = self.api.fetch_period_tickets(origin='MOW', destination='DXB',
+        response = self.air_api.fetch_period_tickets(origin='MOW', destination='DXB',
                                                  beginning_of_period=beginning_of_period)
 
         # Assert that the response indicates success
@@ -70,13 +72,13 @@ class TestAirTicketsApi(TestCase):
         self.assertEqual(response['data'][0]['destination'], expected_data['destination'])
 
     def test_fetch_alternative_route_tickets(self):
-        response = self.api.fetch_alternative_route_tickets(origin='OVB', destination='LED')
+        response = self.air_api.fetch_alternative_route_tickets(origin='OVB', destination='LED')
         # Check if 'prices' key exists in the response and contains at least one item
         assert 'prices' in response, "'prices' key not found in the response."
         assert len(response['prices']) > 0, "No prices found in the response."
 
     def test_fetch_popular_routes_from_city(self):
-        response = self.api.fetch_popular_routes_from_city(origin='MOW')
+        response = self.air_api.fetch_popular_routes_from_city(origin='MOW')
         # Assert that the response indicates success
         self.assertTrue(response['success'])
 
@@ -89,9 +91,17 @@ class TestAirTicketsApi(TestCase):
             os.remove(file_path)
         self.assertFalse(os.path.exists(file_path))
         # check api
-        self.api.fetch_airline_logo(f'{iata_code}')
+        self.air_api.fetch_airline_logo(f'{iata_code}')
         # check for logo existence
         self.assertTrue(os.path.exists(file_path))
+
+    def test_search_hotel_or_location(self):
+        expected_data = {
+            'status' : 'ok'
+        }
+        query='moscow'
+        response = self.hotel_api.search_hotel_or_location(query=query)
+        self.assertEqual(response['status'], expected_data['status'])
 
 if __name__ == '__main__':
     main()
