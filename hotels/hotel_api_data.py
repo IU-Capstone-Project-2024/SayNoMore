cdfb045f0c40a20d95192c37e5bed23895acbca0
Roman Makeev <r.makeev@innopolis.university>
2024-06-12 11:30:31 +0300

    feat: 'hotel_api_data' has been added

---
+++ hotels/hotel_api_data.py
@@ -0,0 +1,9 @@
+api_token = "34e6ab18578d8a897c41aceaf8d6c584"
+
+search_hotel_or_location_url = "https://engine.hotellook.com/api/v2/lookup.json"
+fetch_hotel_prices_url = "https://engine.hotellook.com/api/v2/cache.json"
+fetch_hotel_collections_url = "https://yasen.hotellook.com/tp/public/widget_location_dump.json"
+fetch_hotel_collection_types_url = "https://yasen.hotellook.com/tp/public/available_selections.json"
+fetch_room_types_url = "https://engine.hotellook.com/api/v2/static/roomTypes.json"
+fetch_hotel_types_url = "https://engine.hotellook.com/api/v2/static/hotelTypes.json"
+fetch_hotel_photos_base_url = " https://yasen.hotellook.com/photos/hotel_photos"
