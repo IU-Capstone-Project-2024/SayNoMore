5d58712120a25c15980332a633fc20528ddb01c1
Maxim Martyshov <118848075+ForYourEyesOnlyyy@users.noreply.github.com>
2024-07-03 11:20:14 +0300

    resolve conflicts

--- telegram-bot/utils/messages.py
+++ telegram-bot/utils/messages.py
@@ -1,7 +1,14 @@
-WELCOME_MESSAGE = 'Hi, welcome to SayNoMore bot. Tell us more about the trip you are planning'
-MORE_INFO_MESSAGE = (
-    'It seems that I did not receive all the necessary information for your request. Please specify '
-    'the destination city. Additionally, if you have a budget, you can provide that too, although it '
-    'is not mandatory. Thank you!')
+WELCOME = 'Hi, welcome to SayNoMore bot. Tell us more about the trip you are planning'
+MORE_INFO= ('It seems that I did not receive all the necessary information for your request. Please specify '
+                     'the destination city. Additionally, if you have a budget, you can provide that too, although it '
+                     'is not mandatory. Thank you!')
 
-AWAIT_MESSAGE = 'All good, let me prepare a trip plan for you'
+HELP = (
+    "Getting Started:\n\n"
+    "  1. Start the Bot: Type /start.\n"
+    "  2. Share Your Details: Example: “Going to Moscow, returning July 22nd, budget is 800 thousand.”\n"
+    "  3. Get Recommendations: We’ll give you a detailed trip plan.\n\n"
+    "Ready to plan your next adventure? Start chatting with SayNoMore now! 🎉"
+)
+
+AWAIT = 'All good, let me prepare a trip plan for you'
