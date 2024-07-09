7a775c9d1cb38ce42ca160dfb5214e7c22f3eb92
Maxim Martyshov <maxitux@icloud.com>
2024-07-09 07:45:25 +0000

    fix: delete all exept form stuff

--- api_collector/air_tickets/air_api_data.py
+++
@@ -1,8 +0,0 @@
-api_token = "34e6ab18578d8a897c41aceaf8d6c584"
-
-fetch_cheapest_tickets_url = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates"
-fetch_grouped_tickets_url = "https://api.travelpayouts.com/aviasales/v3/grouped_prices"
-fetch_period_tickets_url = "http://api.travelpayouts.com/aviasales/v3/get_latest_prices"
-fetch_alternative_route_tickets_url = "http://api.travelpayouts.com/v2/prices/nearest-places-matrix"
-fetch_popular_routes_from_city_url = "http://api.travelpayouts.com/v1/city-directions"
-fetch_airline_logos_url_base = "http://pics.avs.io/"
