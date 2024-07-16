a5957a2f87bcc73acec02a994be2bfed75d8b85b
Maxim Martyshov <maxitux@icloud.com>
2024-07-16 08:56:14 +0000

    solve conflicts 2.0

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,9 +1,6 @@
-import telebot
 import sys
-import asyncio
-import threading
 import json
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, LabeledPrice, InputMediaPhoto, WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto, LabeledPrice, WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, PreCheckoutQueryHandler, filters, ContextTypes
 from api_collector.route.route import find_top_routes
 from telegram_bot.utils import messages
@@ -15,8 +12,8 @@ from request_analyzer.llm import LLM
 user_states = {}
 
 class SayNoMoreBot:
-    def __init__(self):
-        self.bot = telebot.TeleBot('7333725090:AAFC6DwjlSs5VvvJ6ML863e5yx8h-NgAR60')
+    def __init__(self, token):
+        self.application = Application.builder().token(token).build()
         self.setup_handlers()
 
     def setup_handlers(self):
@@ -43,26 +40,24 @@ class SayNoMoreBot:
         else:
             await update.message.reply_text(messages.WELCOME_RU)
 
-        @self.bot.message_handler(commands=['help'])
-        def send_help(message):
-            user_id = message.chat.id
-            if user_id not in user_states:
-                user_states[user_id] = self.initialize_user_state()
-            language = user_states[user_id]["language"]
-            if language == "en":
-                self.bot.send_message(message.chat.id, messages.HELP_EN)
-            else:
-                self.bot.send_message(message.chat.id, messages.HELP_RU)
+    async def send_help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        user_id = update.message.chat.id
+        if user_id not in user_states:
+            user_states[user_id] = self.initialize_user_state()
+        language = user_states[user_id]["language"]
+        if language == "en":
+            await update.message.reply_text(messages.HELP_EN)
+        else:
+            await update.message.reply_text(messages.HELP_RU)
 
-        @self.bot.message_handler(commands=['restart'])
-        def restart_trip_planning(message):
-            self.restart_trip_planning_sequence(message.chat.id)
-            user_id = message.chat.id
-            language = user_states[user_id]["language"]
-            if language == "en":
-                self.bot.send_message(message.chat.id, "Trip planning sequence has been restarted. Please start again by providing your trip details.")
-            else:
-                self.bot.send_message(message.chat.id, "Последовательность планирования поездки была перезапущена. Пожалуйста, начните сначала, предоставив детали вашей поездки.")
+    async def restart_trip_planning(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        self.restart_trip_planning_sequence(update.message.chat.id)
+        user_id = update.message.chat.id
+        language = user_states[user_id]["language"]
+        if language == "en":
+            await update.message.reply_text("Trip planning sequence has been restarted. Please start again by providing your trip details.")
+        else:
+            await update.message.reply_text("Последовательность планирования поездки была перезапущена. Пожалуйста, начните сначала, предоставив детали вашей поездки.")
 
     async def choose_language(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await self.send_language_options(update.message.chat.id)
@@ -151,7 +146,7 @@ class SayNoMoreBot:
                 translated_message = user_state["messages"][user_state["step"]]
 
             are_all_fields_retrieved, response_message = await user_state["analyzer"].analyzer_step(translated_message)
-            self.bot.send_message(message.chat.id, response_message)
+            await self.application.bot.send_message(message.chat.id, response_message)
             
             if are_all_fields_retrieved:
                 user_state["completed"] = True
@@ -164,74 +159,32 @@ class SayNoMoreBot:
                     budget=request['Budget']
                 )
                 user_state["routes_list"] = routes_list
-                self.send_routes_with_buttons(message.chat.id, routes_list)
+                await self.send_routes_with_buttons(message.chat.id, routes_list)
             else:
                 user_state["step"] += 1
         else:
-            self.bot.send_message(message.chat.id, 'ERROR')
+            await self.application.bot.send_message(message.chat.id, 'ERROR')
             sys.exit(0)
 
-    def send_routes_with_buttons(self, chat_id, routes_list):
-        markup = types.InlineKeyboardMarkup()
-        for index, route in enumerate(routes_list):
-            markup.add(types.InlineKeyboardButton(text=f"Route {index + 1}", callback_data=f"route_{index}"))
-        
+    async def send_routes_with_buttons(self, chat_id, routes_list):
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=f"Route {index + 1}", callback_data=f"route_{index}") for index, route in enumerate(routes_list)]])
         routes_message = route_list_to_string(routes_list)
-        self.bot.send_message(chat_id, routes_message, reply_markup=markup)
+        await self.application.bot.send_message(chat_id, routes_message, reply_markup=markup)
 
-    def handle_callback_query(self, call):
-        user_id = call.message.chat.id
-        user_state = user_states.get(user_id)
-        if call.data.startswith("route_"):
-            route_index = int(call.data.split('_')[1])
-            user_id = call.message.chat.id
-            user_state = user_states.get(user_id)
-            if user_state:
-                selected_route = user_state['routes_list'][route_index]
-                user_state['selected_route'] = selected_route
-                self.bot.send_message(call.message.chat.id, f"You selected route {route_index + 1}:\n{selected_route.to_string()}")
-                self.send_photos(user_id, selected_route.hotel.photo_urls)
-                self.send_payment_button(call.message.chat.id)
-        elif call.data.startswith("lang_"):
-            self.set_user_language(call.message.chat.id, call.data.split('_')[1])
-            self.bot.send_message(call.message.chat.id, "Language preference updated.")
-        
-        elif call.data.startswith("pay_"):
-            user_state = user_states.get(chat_id)
-            prices = [LabeledPrice(label='Trip Payment', amount=user_state['selected_route'].calculate_total_cost() * 100)]
-
-            # test card details: 4000000000003055
-                                # 12/25
-                                # 000
-            self.bot.send_invoice(
-                call.message.chat.id, 
-                title='Trip Payment',
-                description='Payment for selected trip route',
-                provider_token='1744374395:TEST:045d3bca5efb8ecdb8e0',
-                currency='rub',
-                prices=prices,
-                start_parameter='trip-payment',
-                invoice_payload='trip-payment-payload'
-            )
-
-    def send_photos(self, chat_id, photos):
+    async def send_photos(self, chat_id, photos, context):
         if len(photos) > 5:
             photos = photos[:5]
-        media_group = [types.InputMediaPhoto(media=photo_url) for photo_url in photos]
-        self.bot.send_media_group(chat_id, media_group)
+        media_group = [InputMediaPhoto(media=photo_url) for photo_url in photos]
+        await context.bot.send_media_group(chat_id, media_group)
 
-    def send_payment_button(self, chat_id):
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton(text="Pay", callback_data="pay_"))
-        self.bot.send_message(chat_id, "Click to proceed with payment", reply_markup=markup)
+    async def send_payment_button(self, chat_id, context):
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Pay", callback_data="pay_")]])
+        await context.bot.send_message(chat_id, "Click to proceed with payment", reply_markup=markup)
 
     async def send_invoice(self, chat_id, context):
         user_state = user_states.get(chat_id)
         prices = [LabeledPrice(label='Trip Payment', amount=user_state['selected_route'].calculate_total_cost() * 100)]
 
-            # test card details: 4000000000003055
-                                # 12/25
-                                # 000
         await context.bot.send_invoice(
             chat_id,
             title='Trip Payment',
@@ -269,5 +222,5 @@ class SayNoMoreBot:
         }
 
 if __name__ == "__main__":
-    bot = SayNoMoreBot()
-    threading.Thread(target=bot.run).start()
+    bot = SayNoMoreBot('7333725090:AAFC6DwjlSs5VvvJ6ML863e5yx8h-NgAR60')
+    bot.run()
