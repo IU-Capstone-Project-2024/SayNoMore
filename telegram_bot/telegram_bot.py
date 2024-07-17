05bf7654b03635b7201d161394829dc86327d342
Maxim Martyshov <maxitux@icloud.com>
2024-07-17 07:19:04 +0000

    fix all translations except route

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, InputMe
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, PreCheckoutQueryHandler, filters, ContextTypes
 from api_collector.route.route import find_top_routes
 from telegram_bot.utils import messages
-from telegram_bot.utils.formatting import route_list_to_string, translate_to_russian, format_web_app_data
+from telegram_bot.utils.formatting import route_list_to_string, translate_to_russian, translate_to_english, format_web_app_data
 from request_analyzer.request_analyzer import RequestAnalyzer
 from request_analyzer.llm import LLM
 
@@ -55,9 +55,9 @@ class SayNoMoreBot:
         user_id = update.message.chat.id
         language = user_states[user_id]["language"]
         if language == "en":
-            await update.message.reply_text("Trip planning sequence has been restarted. Please start again by providing your trip details.")
+            await update.message.reply_text(messages.RESTART_EN)
         else:
-            await update.message.reply_text("Последовательность планирования поездки была перезапущена. Пожалуйста, начните сначала, предоставив детали вашей поездки.")
+            await update.message.reply_text(messages.RESTART_RU)
 
     async def choose_language(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await self.send_language_options(update.message.chat.id)
@@ -82,27 +82,46 @@ class SayNoMoreBot:
             if user_state:
                 selected_route = user_state['routes_list'][route_index]
                 user_state['selected_route'] = selected_route
-                await call.message.reply_text(f"You selected route {route_index + 1}:\n{selected_route.to_string()}")
+                if user_states.get(update.callback_query.message.chat.id)['language'] == "en":
+                    await call.message.reply_text(f"{messages.SELECTED_ROUTE_EN} {route_index + 1}:\n{selected_route.to_string()}")
+                else:
+                    await call.message.reply_text(f"{messages.SELECTED_ROUTE_RU} {route_index + 1}:\n{translate_to_russian(selected_route.to_string())}")
                 await self.send_photos(user_id, selected_route.hotel.photo_urls, context)
                 await self.send_payment_button(call.message.chat.id, context)
         elif call.data.startswith("lang_"):
             self.set_user_language(call.message.chat.id, call.data.split('_')[1])
-            await call.message.reply_text("Language preference updated.")
+            if user_state['language'] == 'en':
+                await call.message.reply_text(messages.LANGUAGE_EN)
+            else:
+                await call.message.reply_text(messages.LANGUAGE_RU)
         elif call.data.startswith("pay_"):
             await self.send_invoice(call.message.chat.id, context)
 
     async def checkout(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         query = update.pre_checkout_query
-        await context.bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True, error_message="Payment failed, please try again later.")
+        if user_states.get(update.callback_query.message.chat.id)['language'] == "en":
+            await context.bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True, error_message=messages.PAYMENT_FAIL_EN)
+        else:
+            await context.bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True, error_message=messages.PAYMENT_FAIL_RU)
 
     async def got_payment(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Thank you for your payment!")
+        if user_states.get(update.callback_query.message.chat.id)['language'] == "en":
+            await update.message.reply_text(messages.PAYMENT_SUCC_EN)
+        else:
+            await update.message.reply_text(messages.PAYMENT_SUCC_RU)
 
     async def launch_mini_app(self, chat_id):
         mini_app_url = f"https://say-no-more-mini-app.vercel.app?chatId={chat_id}"
-        button = KeyboardButton(text="Open Trip Form", web_app=WebAppInfo(url=mini_app_url))
+        if user_states.get(chat_id)['language'] == "en":
+            button = KeyboardButton(text=messages.OPEN_TRIP_FORM_EN, web_app=WebAppInfo(url=mini_app_url))
+        else:
+            button = KeyboardButton(text=messages.OPEN_TRIP_FORM_RU, web_app=WebAppInfo(url=mini_app_url))
         keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True, one_time_keyboard=True)
-        await self.application.bot.send_message(chat_id, "Please fill out your trip details using the form below:",
+        if user_states.get(chat_id)['language'] == "en":
+            await self.application.bot.send_message(chat_id, messages.TRIP_FORM_EN,
+                                       reply_markup=keyboard)
+        else:
+            await self.application.bot.send_message(chat_id, messages.TRIP_FORM_RU,
                                        reply_markup=keyboard)
 
     async def handle_web_app_data(self, message):
@@ -146,20 +165,15 @@ class SayNoMoreBot:
                 translated_message = user_state["messages"][user_state["step"]]
 
             are_all_fields_retrieved, response_message = await user_state["analyzer"].analyzer_step(translated_message)
-            await self.application.bot.send_message(message.chat.id, response_message)
+            if user_state['language'] == "en":
+                await self.application.bot.send_message(message.chat.id, translate_to_english(response_message))
+            else:
+                await self.application.bot.send_message(message.chat.id, response_message)
             
             if are_all_fields_retrieved:
                 user_state["completed"] = True
-                request = json.loads(response_message)  # Assuming this is the final analyzed message
-                routes_list = find_top_routes(
-                    origin=request['Departure'],
-                    destination=request['Destination'],
-                    departure_at=request['Arrival'],
-                    return_at=request['Return'],
-                    budget=request['Budget']
-                )
-                user_state["routes_list"] = routes_list
-                await self.send_routes_with_buttons(message.chat.id, routes_list)
+                request = json.loads(response_message)
+                await self.get_routes(user_id, request)
             else:
                 user_state["step"] += 1
         else:
@@ -167,7 +181,10 @@ class SayNoMoreBot:
             sys.exit(0)
 
     async def send_routes_with_buttons(self, chat_id, routes_list):
-        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=f"Route {index + 1}", callback_data=f"route_{index}") for index, route in enumerate(routes_list)]])
+        if user_states.get(chat_id)['language'] == 'en':
+            markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=f"Route {index + 1}", callback_data=f"route_{index}") for index, route in enumerate(routes_list)]])
+        else:
+            markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=f"Маршрут {index + 1}", callback_data=f"route_{index}") for index, route in enumerate(routes_list)]])
         routes_message = route_list_to_string(routes_list)
         await self.application.bot.send_message(chat_id, routes_message, reply_markup=markup)
 
@@ -178,29 +195,44 @@ class SayNoMoreBot:
         await context.bot.send_media_group(chat_id, media_group)
 
     async def send_payment_button(self, chat_id, context):
-        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Pay", callback_data="pay_")]])
-        await context.bot.send_message(chat_id, "Click to proceed with payment", reply_markup=markup)
+        if user_states.get(chat_id)['language'] == "en":
+            markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Pay", callback_data="pay_")]])
+            await context.bot.send_message(chat_id, messages.PAY_EN, reply_markup=markup)
+        else :
+            markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Оплатить", callback_data="pay_")]])
+            await context.bot.send_message(chat_id, messages.PAY_RU, reply_markup=markup)
 
     async def send_invoice(self, chat_id, context):
         user_state = user_states.get(chat_id)
         prices = [LabeledPrice(label='Trip Payment', amount=user_state['selected_route'].calculate_total_cost() * 100)]
-
-        await context.bot.send_invoice(
-            chat_id,
-            title='Trip Payment',
-            description='Payment for selected trip route',
-            payload='trip-payment-payload',
-            provider_token='1744374395:TEST:045d3bca5efb8ecdb8e0',
-            currency='RUB',
-            prices=prices,
-            start_parameter='trip-payment'
-        )
+        if user_state['language'] == "en":
+            await context.bot.send_invoice(
+                chat_id,
+                title='Trip Payment',
+                description='Payment for selected trip route',
+                payload='trip-payment-payload',
+                provider_token='1744374395:TEST:045d3bca5efb8ecdb8e0',
+                currency='RUB',
+                prices=prices,
+                start_parameter='trip-payment'
+            )
+        else :
+            await context.bot.send_invoice(
+                chat_id,
+                title='Оплата Путешествия',
+                description='Оплата выбранного маршрута',
+                payload='trip-payment-payload',
+                provider_token='1744374395:TEST:045d3bca5efb8ecdb8e0',
+                currency='RUB',
+                prices=prices,
+                start_parameter='trip-payment'
+            )
 
     async def send_language_options(self, chat_id):
         markup = InlineKeyboardMarkup([
             [InlineKeyboardButton(text="Русский", callback_data="lang_ru"), InlineKeyboardButton(text="English", callback_data="lang_en")]
         ])
-        await self.application.bot.send_message(chat_id, "Choose your language / Выберите язык:", reply_markup=markup)
+        await self.application.bot.send_message(chat_id, "Выберите язык / Choose your language:", reply_markup=markup)
 
     def set_user_language(self, chat_id, language):
         if chat_id not in user_states:
@@ -218,7 +250,7 @@ class SayNoMoreBot:
             "completed": False,
             "messages": [],
             "routes_list": [],
-            "language": "ru"  # Default language is Russian
+            "language": "ru"
         }
 
 if __name__ == "__main__":
--- telegram_bot/utils/formatting.py
+++ telegram_bot/utils/formatting.py
@@ -1,5 +1,6 @@
 from api_collector.route.route import Route
 from google.cloud import translate_v2 as translate
+from deep_translator import GoogleTranslator
 import os
 import pandas as pd
 from datetime import datetime
@@ -38,11 +39,21 @@ def route_list_to_string(route_list: list[Route]) -> str:
     return result
 
 
-def translate_to_russian(text):
+def translate_to_russian_old(text):
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'telegram_bot/secrets/translationkey.json'
     translate_client = translate.Client()
     result = translate_client.translate(text, target_language='ru')
     return result['translatedText']
+
+def translate_to_russian(text):
+    translator = GoogleTranslator(source='en', target='ru')
+    translation = translator.translate(text)
+    return translation
+
+def translate_to_english(text):
+    translator = GoogleTranslator(source='ru', target='en')
+    translation = translator.translate(text)
+    return translation
 
 def get_iata_code(city: str):
     df = pd.read_csv('data/all_cities_codes.csv')
@@ -50,16 +61,6 @@ def get_iata_code(city: str):
     return city_name_to_code.get(city)
 
 def format_web_app_data(data):
-        {"Arrival": "2024-07-19", "Return": "2024-07-26", "Departure": "LED", "Destination": "KZN", "Budget": "None"}
-
-        {
-            "userId": "",
-            "departure": "T",
-            "destination": "3",
-            "arrival": "2024-07-12",
-            "return": "2024-07-28",
-            "budget": ""
-        }
         request = {}
         request['Arrival'] = datetime.strptime(data['arrival'], "%Y-%m-%d").strftime("%Y-%m-%d")
         request['Return'] = datetime.strptime(data['return'], "%Y-%m-%d").strftime("%Y-%m-%d")
